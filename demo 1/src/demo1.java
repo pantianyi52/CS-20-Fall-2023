e8f9f2adc6c0e6e91214488d89ba44301e30f8c9
1100019824 <1100019824@L815WD220020.edu.cbe.ab.ca>
2023-09-13 11:06:54 -0600

    11:06 am

--- demo 1/src/demo1.java
+++ demo 1/src/demo1.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
 public class demo1
 {
 	public static void main(String[] args)
 	{
-	
+	Scanner input = new Scanner(System.in);
+	System.out.print("What is num1:\n");
+	double num1 = input.nextDouble();
+	System.out.print("What is num2: ");
+	double num2 = input.nextDouble();
+	input.close();
+	double num3 = num1 + num2;
+	System.out.println("The result of num1 and num2 is: " + num3);
 	}
 	
 }
